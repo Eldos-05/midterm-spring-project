eaaa1bfbf79356ad5db736d934c88fa337f84614
Eldos-05 <eldos.220505@gmail.com>
2025-03-12 12:25:03 +0600

    Initial commit: Spring Boot project setup

---
+++ src/test/java/comsep23/midtermspringproject/MidtermSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package comsep23.midtermspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermSpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
